ed86364338fd1a3ee84666d7861a8e22acfcc600
3lcode <3lcode@mail.ru>
2024-11-18 21:57:46 +0300

    улучшение, добавление валидации

--- composeApp/src/commonMain/kotlin/ru/trilcode/pomodoro/ui/settings/SettingsRoute.kt
+++ composeApp/src/commonMain/kotlin/ru/trilcode/pomodoro/ui/settings/SettingsRoute.kt
@@ -5,68 +5,109 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LifecycleEventEffect
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
 import pomodoro.composeapp.generated.resources.Res
 import pomodoro.composeapp.generated.resources.focus_duration
+import pomodoro.composeapp.generated.resources.minute_abbreviated
 import pomodoro.composeapp.generated.resources.rest_duration
+import ru.trilcode.pomodoro.utils.Platform
+import ru.trilcode.pomodoro.utils.Logger
 
 @Composable
 fun SettingsRoute(
-    navController: NavHostController,
     settingsViewModel: SettingsViewModel = koinViewModel()
 ) {
-    Box(Modifier.fillMaxSize()) {
+    val platform = koinInject<Platform>()
+    val focusing by settingsViewModel.settings.focusing.collectAsState()
+    val rest by settingsViewModel.settings.rest.collectAsState()
+
+    LaunchedEffect(focusing) {
+        settingsViewModel.uiState.focusing = focusing.inWholeSeconds.toString() //todo minutes
+    }
+
+    LaunchedEffect(focusing) {
+        settingsViewModel.uiState.rest = rest.inWholeSeconds.toString() //todo minutes
+    }
+
+    LifecycleEventEffect(
+        event = Lifecycle.Event.ON_STOP,
+        onEvent = {
+            settingsViewModel.saveTimes()
+        }
+    )
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(top = if(platform is Platform.Desktop) 60.dp else 0.dp)
+    ) {
         Column {
-            IconButton(
-                modifier = Modifier.align(Alignment.Start),
-                onClick = {
-                    settingsViewModel.saveTimes()
-                    if(navController.currentBackStack.value.size > 2)
-                        navController.navigateUp()
-                }
-            ) {
-                Icon(Icons.AutoMirrored.Filled.ArrowBack, "back")
-            }
             OutlinedTextField(
                 modifier = Modifier
                     .padding(horizontal = 20.dp)
                     .fillMaxWidth(),
-                value = settingsViewModel.focusing,
-                onValueChange = {
-                    settingsViewModel.focusing = it
+                value = settingsViewModel.uiState.focusing,
+                onValueChange = { input->
+                    if(input.all { it.isDigit() })
+                        settingsViewModel.uiState.focusing = input
                 },
                 label = {
                     Text(
                         text = stringResource(Res.string.focus_duration)
                     )
-                }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Number
+                ),
+                suffix = {
+                    Text(stringResource(Res.string.minute_abbreviated))
+                },
+                singleLine = true,
             )
             OutlinedTextField(
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .padding(horizontal = 20.dp)
                     .fillMaxWidth(),
-                value = settingsViewModel.rest,
-                onValueChange = {
-                    settingsViewModel.rest = it
+                value = settingsViewModel.uiState.rest,
+                onValueChange = { input ->
+                    if(input.all { it.isDigit() })
+                        settingsViewModel.uiState.rest = input
                 },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Number
+                ),
                 label = {
                     Text(
                         text = stringResource(Res.string.rest_duration)
                     )
-                }
+                },
+                suffix = {
+                    Text(stringResource(Res.string.minute_abbreviated))
+                },
+                singleLine = true,
             )
         }
     }
--- composeApp/src/commonMain/kotlin/ru/trilcode/pomodoro/ui/settings/SettingsViewModel.kt
+++ composeApp/src/commonMain/kotlin/ru/trilcode/pomodoro/ui/settings/SettingsViewModel.kt
@@ -1,36 +1,45 @@
 package ru.trilcode.pomodoro.ui.settings
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.android.annotation.KoinViewModel
-import org.koin.compose.koinInject
-import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
-import ru.trilcode.pomodoro.ui.timer.CountdownTimerViewModel
-import ru.trilcode.pomodoro.ui.timer.Times
-import kotlin.time.Duration.Companion.minutes
+import ru.trilcode.pomodoro.data.TimesRepository
 import kotlin.time.Duration.Companion.seconds
 
 @KoinViewModel
 class SettingsViewModel(
-   private val times: Times
-) : ViewModel(), KoinComponent {
+    private val timesRepository: TimesRepository,
+   //private val times: Times
+) : ViewModel() {
+    val settings = Settings()
 
-    var focusing by mutableStateOf(times.focusing.inWholeSeconds.toString())
+    val uiState = UiState()
 
-    var rest by mutableStateOf(times.rest.inWholeSeconds.toString())
-
-    fun saveTimes() = CoroutineScope(Dispatchers.IO).launch{
-        times.focusing = focusing.toInt().seconds
-        times.rest = rest.toInt().seconds
+    inner class Settings {
+        val focusing = timesRepository.focusing.stateIn(viewModelScope, SharingStarted.Eagerly, TimesRepository.focusingDefault)
+        val rest = timesRepository.rest.stateIn(viewModelScope, SharingStarted.Eagerly, TimesRepository.restDefault)
     }
 
+    inner class UiState {
+        var focusing by mutableStateOf(TimesRepository.focusingDefault.inWholeSeconds.toString())
+        var rest by mutableStateOf(TimesRepository.restDefault.inWholeSeconds.toString())
+    }
+
+
+    fun saveTimes() {
+        CoroutineScope(Dispatchers.IO).launch {
+            // times.focusing = focusing.toInt().seconds
+            //  times.rest = rest.toInt().seconds
+            timesRepository.updateFocusing(uiState.focusing.toInt().seconds.inWholeSeconds) // todo minutes
+            timesRepository.updateRest(uiState.rest.toInt().seconds.inWholeSeconds) // todo minutes
+        }
+    }
 }
